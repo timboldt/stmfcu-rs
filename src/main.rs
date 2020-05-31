fd78487cfabdf7c83df8122c0a251828921c9ee7
Tim Boldt <tim.boldt@gmail.com>
2020-05-30 17:23:00 -0700

    Basic licensing and repo setup

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,28 @@
+//  Copyright 2020 Google LLC
+//
+//  Licensed under the Apache License, Version 2.0 (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//
+//      https://www.apache.org/licenses/LICENSE-2.0
+//
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
+
 #![no_std]
 #![no_main]
 
-// pick a panicking behavior
-//extern crate panic_halt; // you can put a breakpoint on `rust_begin_unwind` to catch panics
-// extern crate panic_abort; // requires nightly
-// extern crate panic_itm; // logs messages over ITM; requires ITM support
 extern crate panic_semihosting; // logs messages to the host stderr; requires a debugger
 
-use cortex_m::asm;
 use cortex_m_rt::entry;
-use cortex_m_semihosting::{debug, hprintln};
+use cortex_m_semihosting::hprintln;
 
 #[entry]
 fn main() -> ! {
     hprintln!("Hello, world!").unwrap();
-    hprintln!("*STOP*").unwrap();
 
     loop {
         // your code goes here
